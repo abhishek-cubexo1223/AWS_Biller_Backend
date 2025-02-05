2544a3c5c05b7003a8a4ff002285907ac60bac7a
Abhishek Jha <jabhishek10142@gmail.com>
2025-02-05 11:41:00 +0530

    5 feb 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! I am Abhishek Yadav and this is my first NestJS application and I am a full stack developer';
   }
 }
